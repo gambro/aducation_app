556ba25676b1a309d1dbdb351be774f43fcc44f1
gambro <gambro87@gmail.com>
2013-03-29 17:04:21 +0200

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AducationApp::Application.config.secret_token = '0f52eb90997149135ade2c218e69219916ba68b89e40b5e38b12db676916739fc8f35ae973525868c50080aa323ec6ea556776da2acb809bc807e59fb196ebdf'
